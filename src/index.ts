125bfaafe806dc491ce9f249a5f5dd88d383377c
Silva404 <brenosilva0252@gmail.com>
2020-09-23 17:26:08 -0300

    chore: add graphql and apollo

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,38 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./hello";
+import { PostResolver } from "./post";
 
 const main = async () => {
-  const orm = MikroORM.init(microConfig);
-  (await orm).getMigrator().up();
-  const post = (await orm).em.create(Post, { title: "my first post" });
-  (await orm).em.persistAndFlush(post);
+  const orm = await MikroORM.init(microConfig);
+  (await orm)
+    .getMigrator()
+    .up()
+    .catch((err) => {
+      console.error(err);
+    });
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em})
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(3000, () => {
+    console.log("server is running");
+  });
 };
 
 main().catch((err) => {
-  console.error(err); 
+  console.error(err);
 });
